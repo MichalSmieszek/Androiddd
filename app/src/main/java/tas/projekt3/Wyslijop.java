86929f0d4790c80c17e49581dd5110436deabb82
Michal <wowspam0@gmail.com>
2018-01-25 17:00:02 +0100

    koniec :P

--- app/src/main/java/tas/projekt3/Wyslijop.java
+++ app/src/main/java/tas/projekt3/Wyslijop.java
@@ -5,6 +5,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import org.json.*;
@@ -19,7 +21,8 @@ import cz.msebera.android.httpclient.Header;
 
 public class Wyslijop extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
-        final EditText nick,haslo,produkt,zaleta;
+        final EditText nick,haslo,produkt,zaleta,wada,opinia;
+        final RatingBar ocena;
         Button button;
         final AsyncHttpClient client = new AsyncHttpClient();
         super.onCreate(savedInstanceState);
@@ -29,12 +32,19 @@ public class Wyslijop extends ActionBarActivity {
         nick = (EditText) findViewById(R.id.editText6);
         haslo = (EditText) findViewById(R.id.editText7);
         zaleta = (EditText) findViewById(R.id.editText9);
+        wada=(EditText)findViewById(R.id.editText10);
+        ocena=(RatingBar)findViewById(R.id.ratingBar);
+        opinia=(EditText)findViewById((R.id.editText11));
        button.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
 
                 RequestParams params = new RequestParams();
-                params.put("product", produkt.getText().toString());
+                params.put("userId","1");
+                params.put("productId", produkt.getText());
                 params.put("pros",zaleta.getText().toString());
+                params.put("opinionText",opinia.getText().toString());
+                params.put("score", ((int) ocena.getRating()));
+                params.put("cons",wada.getText().toString());
                 client.setBasicAuth(nick.getText().toString(),haslo.getText().toString());
                 client.get("http://uam.grzegorz2047.pl:8080/opinions/add", params, new JsonHttpResponseHandler() {
                     @Override
